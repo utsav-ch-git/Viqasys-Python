d3e1b2a5f8c4994d734780e5f18f0dfb6689f11e
utsav <choudhuryutsav@outlook.com>
2024-03-10 18:46:43 +0530

    docstring Demo

---
+++ docStringDemo.py
@@ -0,0 +1,6 @@
+def fullName(fname,lname):
+    '''This function Concatenates First Name & last Name'''
+    print("Your Full Name Is : "+fname+" "+lname)
+
+fullName("Israt","Jaha")
+print(fullName.__doc__)
